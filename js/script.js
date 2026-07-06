fcc76d0d3fceb3bc7f5218631b6a116d607d37e2
jADE-u <supapornja2212@gmail.com>
2026-07-06 14:02:11 +0700

    Update mobile menu

--- js/script.js
+++ js/script.js
@@ -13,3 +13,32 @@ document.getElementById("loader").style.display="none";
 },2000);
 
 });
+const menuBtn=document.getElementById("menu-toggle");
+const nav=document.getElementById("nav-menu");
+
+menuBtn.addEventListener("click",()=>{
+
+nav.classList.toggle("active");
+
+if(nav.classList.contains("active")){
+
+menuBtn.innerHTML='<i class="fas fa-times"></i>';
+
+}else{
+
+menuBtn.innerHTML='<i class="fas fa-bars"></i>';
+
+}
+
+});
+document.querySelectorAll("#nav-menu a").forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+nav.classList.remove("active");
+
+menuBtn.innerHTML='<i class="fas fa-bars"></i>';
+
+});
+
+});
